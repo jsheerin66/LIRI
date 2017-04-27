3f89a0a5aafeb2c136774dd602ac41c02737f85e
jsheerin66 <justinsheerin@gmail.com>
2017-04-27 19:27:43 -0400

    grabbing song from spotify
    was able to grab the song from spotify. next i want to grab more
    objects

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
+var twitterKeys = {
   consumer_key: 'jalOnd3YYfMzvoLP7YWXiubKb',
   consumer_secret: 'vIEHSHzZb7VyFFkbpuHbB2Slz7OzKEbHfVej51yyMGB5xxvnCG',
-  access_token_key: '	1378303370-TU0reAjmCN9oYgIykg1TIx81g9he9LAwixa1ca3',
+  access_token_key: '1378303370-TU0reAjmCN9oYgIykg1TIx81g9he9LAwixa1ca3',
   access_token_secret: '3sXLNtUJioUlunVGCePrYwmxRqECkCCoZxwpribkjMP3F',
 }
 
@@ -14,4 +14,4 @@ exports.twitterKeys = {
 // access_token_key: '	1378303370-TU0reAjmCN9oYgIykg1TIx81g9he9LAwixa1ca3',
 // access_token_secret: '3sXLNtUJioUlunVGCePrYwmxRqECkCCoZxwpribkjMP3F',
 // }
-// module.exports = twitterKeys
+module.exports = twitterKeys
--- liri.js
+++ liri.js
@@ -1,17 +1,92 @@
 var twitterKeys = require("./keys.js");
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+
+var client = new Twitter({
+    consumer_key: twitterKeys.consumer_key,
+    consumer_secret: twitterKeys.consumer_secret,
+    access_token_key: twitterKeys.access_token_key,
+    access_token_secret: twitterKeys.access_token_secret
+});
+
+var params = {
+    screen_name: 'JustinSheerin'
+};
+// pullTweets()
+
+
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     console.log("line 15 " + tweets);
+//   }
+// });
+
+
+
+console.log("twitterKeysis ")
 console.log(twitterKeys)
+console.log(twitterKeys.consumer_secret)
+console.log(twitterKeys.consumer_key)
+console.log(twitterKeys.access_token_key)
+console.log(twitterKeys.access_token_secret)
 var command = process.argv[2];
-console.log(command)
+var query = process.argv[3];
+console.log(query)
+console.log("commandIs " + command)
 
 if (command == "my-tweets") {
-  console.log("my-tweets")
+    console.log("my-tweets")
+    pullTweets()
+} else if (command == "spotify-this-song") {
+    console.log("spotify-this-song")
+    pullSpotify(query)
+} else if (command == "movie-this") {
+    console.log("movie-this")
+} else if (command == "do-what-it-says") {
+    console.log("do-what-it-says")
 }
-else if (command == "spotify-this-song"){
-  console.log("spotify-this-song")
+//functions
+
+function pullTweets() {
+    console.log("thisIsPullTweets")
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("thisIsTweets");
+            // console.log(tweets[0]);
+            // console.log(tweets[0].text);
+            // console.log(response);
+            for (i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text)
+            }
+        }
+    });
+
 }
-else if (command == "movie-this"){
-  console.log("movie-this")
+
+function pullSpotify(song) {
+    console.log(song)
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        // console.log(data)
+        // console.log(data.tracks)
+
+        for (i = 0; i < data.tracks.items.length; i++) {
+            console.log(data.tracks.items[i].name)
+        }
+        // Do something with 'data'
+    });
 }
-else if (command == "do-what-it-says"){
-  console.log("do-what-it-says")
+
+function pullRequest() {
+  console.log(pullRequest)
+
 }
